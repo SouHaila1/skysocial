0e6cf8a8d376be5e6c44c13ab98dc192ea907867
SaadJmari <saad.jmari.1999@gmail.com>
2020-04-20 19:49:58 +0000

    modification

--- skysocial/resources/views/welcome.blade.php
+++ skysocial/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Saad
                 </div>
 
                 <div class="links">
