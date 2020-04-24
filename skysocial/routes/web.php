477c7001566358bfca44fd4e424a83ff84b33459
SaadJmari <saad.jmari.1999@gmail.com>
2020-04-24 16:41:06 +0000

    profile page

--- skysocial/routes/web.php
+++ skysocial/routes/web.php
@@ -20,3 +20,6 @@ Route::post('/','registerController@store');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile',function(){
+    return view('profile');
+});
