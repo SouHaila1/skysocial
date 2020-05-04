9d4dc32a4b462e697f9992e89bf0eacc2679afe6
SouHaila1 <63099847+SouHaila1@users.noreply.github.com>
2020-05-04 17:22:32 +0000

    post

--- skysocial/index.php
+++ skysocial/index.php
@@ -1,41 +1,40 @@
-<?php  
-require 'config/config.php';
-?>
+<?php 
+include("includes/header.php");
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SkySocial</title>
-    
-    <style>
-        body{
-            margin:0px;
-            margin-top:0px;
-            padding:0px;
-            padding-top:0px;
-        }
-        .navbar{
-            margin:0px;
-            margin-top:0px;
-            padding:0px;
-            padding-top:0px;
-        }
-        
+ ?>
+	<div class="user_details column">
+		<a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
 
-    </style>
-</head>
-<body>
-<nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background:#87cefa ">
-            <div class="container">
-                <ul class="navbar-nav mr-auto">
-                    <img src="assets/images/logo/logoWelcome.png" style="width:15rem; ">  
-                </ul>
-                <ul class="navbar-nav ml-auto">
+		<div class="user_details_left_right">
+			<a href="<?php echo $userLoggedIn; ?>">
+			<?php 
+			echo $user['first_name'] . " " . $user['last_name'];
 
-                </ul>
-                    
-            </div>
+			 ?>
+			</a>
+			<br>
+			<?php echo "Posts: " . $user['num_posts']. "<br>"; 
+			echo "Likes: " . $user['num_likes'];
+            echo "Likes: " . $user['username'];
+			?>
+		</div>
+
+	</div>
+
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="Write Post "></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<hr>
+
+		</form>
+
+		
+
+
+	</div>
+
+
+	</div>
 </body>
 </html>
