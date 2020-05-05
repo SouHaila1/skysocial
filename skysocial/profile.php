771744f3a9e0526b4fc3155559761e7d83b4a68e
SaadJmari <saad.jmari.1999@gmail.com>
2020-05-05 18:34:27 +0000

    profile update

--- skysocial/profile.php
+++ skysocial/profile.php
@@ -1,94 +1,26 @@
 <?php 
 include("includes/header.php");
+require("includes/classes/User.php");
 
-if(isset($_GET['profile_username'])){
-    $username = $_GET['profile_username'];
-    $user_details_query =  mysqli_query($con, "SELECT * FROM users WHERE username = '$username'");
-    $user_array = mysqli_fetch_array($user_details_query);
-}
-?>
-<link rel="stylesheet" href="assets/css/profile.css">
-
-<!--Profile Body-->
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="fb-profile-block">
-                <div class="fb-profile-block-thumb cover-container"></div>
-                <div class="profile-img">
-                    <a href="#">
-                        <img src="assets\images\profile_pics\defaults\head_carrot.png" alt="" title="">        
-                    </a>
-                </div>
-                <div class="profile-name">
-                   <h2><?php $username ?></h2>
-                </div>
-                
-                <div class="fb-profile-block-menu">
-                    <div class="block-menu">
-                        <ul>
-                            <li><a href="#timeline" onclick="timeline()">Timeline</a></li>
-                            <li><a href="#friends" onclick="friends()">Friends</a></li>
-                            <li><a href="#photos" onclick="photos()">Photos</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="divider"></div>
-<div class="container">
-    <div id="timeline">
-        <div class="card gedf-card" >
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">Make
-                            a publication</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="images-tab" data-toggle="tab" role="tab" aria-controls="images" aria-selected="false" href="#images">Images</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="card-body">
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
-                        <div class="form-group">
-                            <label class="sr-only" for="message">post</label>
-                            <textarea class="form-control" id="message" rows="3" placeholder="What are you thinking?"></textarea>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="images" role="tabpanel" aria-labelledby="images-tab">
-                        <div class="form-group">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile">
-                                <label class="custom-file-label" for="customFile">Upload image</label>
-                            </div>
-                        </div>
-                        <div class="py-4"></div>
-                    </div>
-                </div>
-                <div class="btn-toolbar justify-content-between">
-                    <div class="btn-group">
-                        <button type="submit" class="btn btn-primary">share</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="friends">
-        <h1>this is friends div</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-    </div>
-    <div id="photos">
-        <h1>this is photos div</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-    </div>
-</div>
+if(isset($_GET['profile_username'])) {
+	$username = $_GET['profile_username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+	$user_array = mysqli_fetch_array($user_details_query);
 
 	$num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+}
+
+if(isset($_POST['remove_friend'])) {
+	$user = new User($con, $userLoggedIn);
+	$user->removeFriend($username);
+}
+
+if(isset($_POST['add_friend'])) {
+	$user = new User($con, $userLoggedIn);
+	$user->sendRequest($username);
+}
+if(isset($_POST['respond_request'])) {
+	header("Location: requests.php");
 }
  ?>
 
@@ -127,6 +59,32 @@ if(isset($_GET['profile_username'])){
 	top: 30px;
 	padding-left: 0px;
 }
+.danger{
+	background-color: #e74c3c;
+}
+.warning{
+	background-color: #f0ad4e;
+}
+.default{
+	background-color: #bdc3c7;
+}
+.success{
+	background-color: #2ecc71;
+}
+.info{
+	background-color: #3498db;
+}
+.deep_blue{
+	background-color: #0043f0;
+}
+.profile_left input[type="submit"] {
+	width: 90%;
+	height: 30px;
+	border-radius: 5px;
+	margin: 7px 0 0 7px;
+	border: none;
+	color: #fff;
+}
 </style>
 	 
 
@@ -138,13 +96,39 @@ if(isset($_GET['profile_username'])){
  			<p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
  			<p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
  			<p><?php echo "Friends: " . $num_friends ?></p>
- 		</div>
-	</div>
-	<form action="<?php echo '$username'?>">
+		 </div>
+		 <form action="<?php echo '$username'?>">
 		<?php 
-		$profile_user_obj = new User($con, $username);
-        ?>
+		$profile_user_obj = new User($con, $username); 
+		if($profile_user_obj->isClosed()) {
+			header("Location: user_closed.php");
+		}
+
+		$logged_in_user_obj = new User($con, $userLoggedIn); 
+
+		if($userLoggedIn != $username) {
+
+			if($logged_in_user_obj->isFriend($username)) {
+				echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+			}
+			else if ($logged_in_user_obj->didReceiveRequest($username)) {
+				echo '<input type="submit" name="respond_request" class="warning" value="Respond to Request"><br>';
+			}
+			else if ($logged_in_user_obj->didSendRequest($username)) {
+				echo '<input type="submit" name="" class="default" value="Request Sent"><br>';
+			}
+			else 
+				echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
+
+		}
+
+		?>
 	</form>
+	</div>
+	<div class="main_column column">
+		<?php echo $username ; ?>
+	</div>
+	
 </div>
 </body>
 </html>
