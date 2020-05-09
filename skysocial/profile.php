ec5e3abb42cf12b0aae8f74f3f3533a8670a0b29
SouHaila1 <63099847+SouHaila1@users.noreply.github.com>
2020-05-09 16:59:34 +0000

    image

--- skysocial/profile.php
+++ skysocial/profile.php
@@ -22,6 +22,50 @@ if(isset($_POST['add_friend'])) {
 if(isset($_POST['respond_request'])) {
 	header("Location: requests.php");
 }
+if(isset($_POST['post'])){
+
+	$uploadOk = 1;
+	$imageName = $_FILES['fileToUpload']['name'];
+	$errorMessage = "";
+
+	if($imageName != "") {
+		$targetDir = "assets/images/posts/";
+		$imageName = $targetDir . uniqid() . basename($imageName);
+		$imageFileType = pathinfo($imageName, PATHINFO_EXTENSION);
+
+		if($_FILES['fileToUpload']['size'] > 10000000) {
+			$errorMessage = "Sorry your file is too large";
+			$uploadOk = 0;
+		}
+
+		if(strtolower($imageFileType) != "jpeg" && strtolower($imageFileType) != "png" && strtolower($imageFileType) != "jpg") {
+			$errorMessage = "Sorry, only jpeg, jpg and png files are allowed";
+			$uploadOk = 0;
+		}
+
+		if($uploadOk) {
+			if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $imageName)) {
+				//image uploaded okay
+			}
+			else {
+				//image did not upload
+				$uploadOk = 0;
+			}
+		}
+
+	}
+
+	if($uploadOk) {
+		$post = new Post($con, $userLoggedIn);
+		$post->submitPost($_POST['post_text'], 'none', $imageName);
+	}
+	else {
+		echo "<div style='text-align:center;' class='alert alert-danger'>
+				$errorMessage
+			</div>";
+	}
+
+}
  ?>
 
 <style type="text/css">
@@ -144,6 +188,7 @@ if(isset($_POST['respond_request'])) {
 
     ?>
 
+
  	</div>
 	 <div class="main_column column">
 		<form class="post_form" action="index.php" method="POST" enctype="multipart/form-data">
@@ -258,6 +303,64 @@ if(isset($_POST['respond_request'])) {
   });
 
   </script>
+<script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$('#loading').show();
+
+		//Original ajax request for loading first posts 
+		$.ajax({
+			url: "includes/handlers/ajax_load_posts.php",
+			type: "POST",
+			data: "page=1&userLoggedIn=" + userLoggedIn,
+			cache:false,
+
+			success: function(data) {
+				$('#loading').hide();
+				$('.posts_area').html(data);
+			}
+		});
+
+		$(window).scroll(function() {
+		//$('#load_more').on("click", function() {
+
+			var height = $('.posts_area').height(); //Div containing posts
+			var scroll_top = $(this).scrollTop();
+			var page = $('.posts_area').find('.nextPage').val();
+			var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+			if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+			//if (noMorePosts == 'false') {
+				$('#loading').show();
+
+				var ajaxReq = $.ajax({
+					url: "includes/handlers/ajax_load_posts.php",
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache:false,
+
+					success: function(response) {
+						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
+
+						$('#loading').hide();
+						$('.posts_area').append(response);
+					}
+				});
+
+			} //End if 
+
+			return false;
+
+		}); //End (window).scroll(function())
+
+
+	});
+
+	</script>
 
 
 
